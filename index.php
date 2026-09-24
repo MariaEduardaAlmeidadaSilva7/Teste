76ea4f87c3093e0517f9f47f792b5414d7a481c9
MariaEduardaAlmeidadaSilva7 <mariaeduardaalmeidadasilva110@gmail>
2026-09-24 15:51:09 -0300

    Adiciona matricula e email do aluno

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 $nome = "Maria Eduarda Almeida";
 $turma = "2ºA";
 $curso = "Desenvolvimento de sistemas";
+$matricula = "681768";
+$email = "mariaeduardaalmeidadasilva110@gmail.com";
 
 echo "<hl>Sistema de Cadastro de Alunos</hl>"; 
 echo "<br>"; echo "<br>";
@@ -11,5 +13,10 @@ echo "<br>";
 echo "Turma: " . $turma;
 echo "<br>";
 echo "Curso: " . $curso;
+echo "<br>";
+echo "Matrícula: " . $matricula;
+echo "<br>";
+echo "E-mail: " . $email;
+echo "<br>;"
 
 ?>
