70fb5fa2119762b0f7607ab0b422d6ce826343bc
MariaEduardaAlmeidadaSilva7 <mariaeduardaalmeidadasilva110@gmail>
2026-09-24 14:36:31 -0300

    Cria estrutura inicial do projeto

---
+++ README.md
@@ -0,0 +1,13 @@
+# Sistema de Cadastro de Alunos
+
+##Objetivo
+Criar um sistema de cadastro e consulta de alunos.
+
+##Tecnologias 
+-PHP
+-HTML
+-Git
+-GitHub
+
+##Desenvolvedor
+Maria Eduarda Almeida da Silva 
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+$nome = "Maria Eduarda Almeida";
+$turma = "2ºA";
+$curso = "Desenvolvimento de sistemas";
+
+echo "<hl>Sistema de Cadastro de Alunos</hl>"; 
+echo "<br>"; echo "<br>";
+echo "Aluno: " . $nome;
+echo "<br>";
+echo "Turma: " . $turma;
+echo "<br>";
+echo "Curso: " . $curso;
+
+?>
